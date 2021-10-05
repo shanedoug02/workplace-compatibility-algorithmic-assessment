6ab6169eb2e5cfa0421eb24020646a2e710e9e50
Shane Dougherty <shane@caughtupgoods.com>
2021-10-04 19:47:22 -0600

    Added methods to retrieve the presumed video category based on a search query

--- src/components/VideoContainer.vue
+++ src/components/VideoContainer.vue
@@ -20,34 +20,45 @@ export default {
 	name: 'VideoContainer',
 	data() {
 		return {
-			allVideosArrData: [], // searching for a topic on Youtube returns related videos/data. BUT the videos category id's are not included.  You need to plug in a specific video id to get that video's categoryId number.
+			allVideosArrData: [], // Searching for a topic on Youtube returns related videos/data. BUT the video's category id's are not included.  You need to plug in a specific video id to get that video's categoryId number.
 			slicedVideosArr: [],
-			videoCategoryStringsArr: [],
-			videoCategoryNumbersArr: [],
+			videoCategoryArr: [],
+			mostCommonIdCategory: '',
 		};
 	},
 
 	methods: {
 		requestIdCategory(arr) {
-			//this second API request sends each videos ID to get back each video category ID
+			//This is the second API request (first API request is @ mounted()). It sends each video ID to get back each video CATEGORY ID
 			arr.forEach((element) => {
 				fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${element.id.videoId}&key=AIzaSyAP_2CK1-y8YPJCJ61Vf0iJXfZCg1ppxHY`)
 					.then((response) => response.json())
 					.then((rawDataFromEachVideoId) => {
-						this.videoCategoryStringsArr.push(rawDataFromEachVideoId.items[0].snippet.categoryId);
-						this.stringToNum(this.videoCategoryStringsArr);
-						console.log('THESE ARE ALL THE VIDEO CATEGORY IDS', this.videoCategoryNumbersArr);
+						this.videoCategoryArr.push(rawDataFromEachVideoId.items[0].snippet.categoryId);
+						this.createObjFromArray(this.videoCategoryArr);
 					});
 			});
 		},
-		stringToNum(arr) {
-			// the videoCategoriesArr values begin as strings.  Here they are converted to numbers
-			arr.forEach((element) => {
-				let toNum = null;
-				toNum = Number(element);
-				this.videoCategoryNumbersArr.push(toNum);
-			});
+		createObjFromArray(arr) {
+			//Intended to easily group the most common category ID's as KEYS
+			const idCounter = {};
+			for (let item of arr) {
+				if (idCounter[item]) {
+					idCounter[item]++;
+				} else {
+					idCounter[item] = 1;
+				}
+			}
+			this.findMostCommonIdCategory(idCounter);
 		},
+		findMostCommonIdCategory(counter) {
+			let highestRepeatedKey = Math.max.apply(null, Object.values(counter)); // tells us how many TIMES an objects most popular key was utilized
+			this.mostCommonIdCategory = Object.keys(counter).find((key) => counter[key] === highestRepeatedKey); // returns the most common KEY (i.e. video category)
+			console.log('this.mostCommonIdCategory', this.mostCommonIdCategory);
+			return this.mostCommonIdCategory;
+		},
+
+		//need to plug in a seamless object tag????????
 		createSeamlessUrl() {
 			for (let video of this.slicedVideosArr) {
 				console.log(video);
@@ -56,11 +67,11 @@ export default {
 	},
 	mounted() {
 		//This first API request fetchs 50 YouTube videos and renders 5 to the DOM
-		fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&q=cat&maxResults=50&key=AIzaSyAP_2CK1-y8YPJCJ61Vf0iJXfZCg1ppxHY')
+		fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&q=lakers&maxResults=50&key=AIzaSyAP_2CK1-y8YPJCJ61Vf0iJXfZCg1ppxHY')
 			.then((response) => response.json())
 			.then((rawJson) => {
 				this.allVideosArrData = rawJson.items;
-				console.log('this is Allllllllllllll the raw JSON', this.allVideosArrData);
+				console.log('this is Allllllllllllll the raw JSON', this.allVideosArrData, this.allVideosArrData.length);
 				this.slicedVideosArr = rawJson.items.slice(0, 5);
 				console.log('this is the jsonData sliced', this.slicedVideosArr);
 			});
